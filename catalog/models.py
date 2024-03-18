dbc5aeb425bcfc864750a9ac9bf9dad190b1c6fc
aleksandr <89191215757@mail.ru>
2024-03-18 10:29:46 +0500

    Add catalog.models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Категория')
+    description = models.TextField(verbose_name='Описание', **NULLABLE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+        ordering = ('name',)
+
+
+class Product(models.Model):
+    COUNTRY = [
+        (1, 'Россия'),
+        (2, 'Беларусь'),
+        (3, 'Китай'),
+    ]
+
+    name = models.CharField(max_length=100, verbose_name='Продукт')
+    description = models.TextField(verbose_name='Описание', **NULLABLE)
+    img = models.ImageField(upload_to='products_image', verbose_name='Изображение', **NULLABLE)
+    category = models.ForeignKey('catalog.Category', on_delete=models.SET_NULL, verbose_name='Категория', **NULLABLE)
+    price = models.PositiveIntegerField(verbose_name='Цена')
+    in_stock = models.BooleanField(default=True, verbose_name='Наличие')
+    country = models.CharField(choices=COUNTRY, verbose_name='Страна производства', **NULLABLE)
+    created_at = models.DateField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateField(auto_now=True, verbose_name='Дата изменения')
+
+    def __str__(self):
+        return f'{self.name} {self.in_stock}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ('name', 'price',)
