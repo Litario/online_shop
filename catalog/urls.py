3eef7015eb7d5c182a87e119a2b0fb257a2f6687
aleksandr <89191215757@mail.ru>
2024-03-28 12:32:13 +0500

    Add categories.html. Create base template.

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts
+from catalog.views import home, categories, contacts
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', home, name='home'),
+    path('categories/', categories, name='categories'),
     path('contacts/', contacts, name='contacts'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render
 
-from catalog.models import Category
+from catalog.models import Category, Product
 
 
 # Create your views here.
 def home(request):
+    categories = Category.objects.all()[:4]
     context = {
-        'object_list': Category.objects.all()
+        'object_list': categories,
+        'title_1': 'MyShop',
+        'title_2': 'MyShop - это просто еще один онлайн магазин. Не более того.',
     }
     return render(request, "catalog/home.html", context)
+
+
+def categories(request):
+    ctgs = Category.objects.all()
+    context = {
+        'object_list': ctgs,
+        'title_1': 'Категории',
+        'title_2': 'Все категории товаров магазина',
+    }
+    return render(request, "catalog/categories.html", context)
 
 
 def contacts(request):
